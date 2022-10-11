adc95b4848ad8ca2a90f32976401d44995aa249d
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2022-10-10 22:47:23 -0400

    Added DataAccess layer init

---
+++ MiasoftNanus.Phonebook.DataAccess.Test/Models/DiaryTest.cs
@@ -0,0 +1,25 @@
+﻿namespace MiasoftNanus.Phonebook.DataAccess.Test.Models;
+
+using MiasoftNanus.Phonebook.DataAccess.Models;
+
+[TestClass]
+public class DiaryTest
+{
+    [TestMethod]
+    public void VerifiesIfDiaryObjectIsCreated()
+    {
+        var diary = new Diary()
+        {
+            DiaryId = Guid.NewGuid(),
+            Code = "Code-1",
+            Email = "example@miasoftnanus.com",
+            Enable = true,
+            ImagePath = "../img/image1.jpg",
+            DirectLine = "33493949",
+            Internal = "345",
+            Corporate = "77345634",
+        };
+
+        Assert.IsNotNull(diary);
+    }
+}
---
+++ MiasoftNanus.Phonebook.DataAccess/Models/Diary.cs
@@ -0,0 +1,17 @@
+﻿namespace MiasoftNanus.Phonebook.DataAccess.Models;
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("Diary")]
+public class Diary
+{
+    public Guid DiaryId { get; set; }
+    public string? Code { get; set; }
+    public string? Email { get; set; }
+    public bool Enable { get; set; }
+    public string? ImagePath { get; set; }
+    public string? DirectLine { get; set; }
+    public string? Internal { get; set; }
+    public string? Corporate { get; set; }
+}
+
